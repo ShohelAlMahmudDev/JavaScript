432b8195e958c6e6a6c172b418a603dff0aaa439
ShohelAlMahmudDev <shohelcse@yahoo.com>
2024-10-21 00:58:32 +0200

    Arithmetic Operators has been implemented1.

--- operators.js
+++ operators.js
@@ -7,18 +7,19 @@
 let x= 20;
 let y= 10;
 
-console.log(x+y);
-console.log(x-y);
-console.log(x*y);
-console.log(x/y);
-console.log(x%y);
-console.log(x**y);
+console.log(x+y);//Addition
+console.log(x-y);//Subtruction
+console.log(x*y);//Multiplications
+console.log(x/y);//Division
+console.log(x%y);//Remender in division
+console.log(x**y); //Exponentiation x to the power of y
 
 //Incremental Operators(++)
 console.log(x++);
 console.log(x);
 
 console.log(y++);
+console.log(y);
 //Decremental Operators(--)
 console.log(--x);
 console.log(x);
