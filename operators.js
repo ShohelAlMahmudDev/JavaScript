91e74b65061b31c97d102f34dd057c9ea6cf2792
ShohelAlMahmudDev <shohelcse@yahoo.com>
2024-10-21 00:53:02 +0200

    Arithmetic Operators has been implemented.

--- operators.js
+++ operators.js
@@ -3,3 +3,24 @@
 //Flow of operators
 //Variables------------------>Expressions--------------->Algorithms
 
+
+let x= 20;
+let y= 10;
+
+console.log(x+y);
+console.log(x-y);
+console.log(x*y);
+console.log(x/y);
+console.log(x%y);
+console.log(x**y);
+
+//Incremental Operators(++)
+console.log(x++);
+console.log(x);
+
+console.log(y++);
+//Decremental Operators(--)
+console.log(--x);
+console.log(x);
+console.log(--y);
+console.log(y);
